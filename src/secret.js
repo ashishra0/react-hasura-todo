ffd325083bc00ead5fa10611c8ae9b8f51d6359c
Ashish Rao <ashishrao2598@gmail.com>
2019-11-09 15:57:43 +0530

    initial commit

---
+++ src/secret.js
@@ -0,0 +1,3 @@
+export const GRAPHQL_URL = process.env.REACT_APP_GRAPHQL_URI
+export const AUTH_CLIENT_ID = process.env.REACT_APP_AUTH_CLIENT_ID
+export const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN
