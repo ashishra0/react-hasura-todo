72bb1fb15e52ef4b65fbdbc208899141ff3ba106
Ashish Rao <ashishrao2598@gmail.com>
2019-11-09 15:59:37 +0530

    add auth variables

---
+++ src/Auth/auth-vars.js
@@ -0,0 +1,10 @@
+import {
+  AUTH_DOMAIN,
+  AUTH_CLIENT_ID
+} from '../secret';
+
+export const AUTH_KEYS = {
+  domain: AUTH_DOMAIN,
+  clientId: AUTH_CLIENT_ID,
+  callbackUrl: 'http://localhost:3000/callback'
+};
