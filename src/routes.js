3913b62a7803448777bd2556e1fef9d61f05af69
Ashish Rao <ashishrao2598@gmail.com>
2019-11-11 16:48:46 +0530

    add missing imports

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, Router } from 'react-router-dom';
 import App from './App';
+import Home from './Components/Home';
+import Callback from './Components/Callback';
 import Auth from './Auth/Auth';
 import history from './history';
-
+import GetAllTodos from './Components/GetAllTodos';
 
 const auth = new Auth();
 
@@ -18,6 +20,12 @@ export const makeMainRoutes = () => {
     <Router history={history} component={App}>
       <div>
         <Route path="/" render={(props) => <App auth={auth} {...props} />} />
+        <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
+        <Route path="/alltodos" render={(props) => <GetAllTodos auth={auth} {...props} />} />
+        <Route path="/callback" render={(props) => {
+          handleAuthentication(props);
+          return <Callback {...props} />
+        }} />
       </div>
     </Router>
   );
