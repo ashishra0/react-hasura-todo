730bd23c35d2c2498fe64919956ee34dcc4f1533
Ashish Rao <ashishrao2598@gmail.com>
2019-11-12 23:55:01 +0530

    add callback uri from .env

--- src/secret.js
+++ src/secret.js
@@ -1,3 +1,4 @@
 export const GRAPHQL_URL = process.env.REACT_APP_GRAPHQL_URI
 export const AUTH_CLIENT_ID = process.env.REACT_APP_AUTH_CLIENT_ID
 export const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN
+export const AUTH_CALLBACK_URI=process.env.REACT_APP_AUTH_CALLBACK_URI
